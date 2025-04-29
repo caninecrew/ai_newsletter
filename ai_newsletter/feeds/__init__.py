45e08ad3610b916162dfc748fc47f35108790ba0
caninecrew <samuelleerumbley@gmail.com>
2025-04-29 18:10:37 -0500

    feat: Update feed package exports to include GNewsAPI and filtering functions

--- ai_newsletter/feeds/__init__.py
+++ ai_newsletter/feeds/__init__.py
@@ -1,13 +1,18 @@
-"""Feed module exports."""
-from ai_newsletter.feeds.fetcher import (
-    fetch_articles_from_all_feeds,
-    safe_fetch_news_articles
+"""Feed package exports."""
+from ai_newsletter.feeds.gnews_client import GNewsAPI, GNewsAPIError
+from ai_newsletter.feeds.fetcher import fetch_articles_from_all_feeds, safe_fetch_news_articles
+from ai_newsletter.feeds.filters import (
+    filter_articles_by_date,
+    deduplicate_articles,
+    is_duplicate
 )
-from ai_newsletter.feeds.gnews_api import GNewsAPI, GNewsAPIError
 
 __all__ = [
+    'GNewsAPI',
+    'GNewsAPIError',
     'fetch_articles_from_all_feeds',
     'safe_fetch_news_articles',
-    'GNewsAPI',
-    'GNewsAPIError'
+    'filter_articles_by_date',
+    'deduplicate_articles',
+    'is_duplicate'
 ]
