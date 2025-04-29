8bea7dc3f49360a74a5052308fff6bfd0380315c
caninecrew <samuelleerumbley@gmail.com>
2025-04-29 18:10:21 -0500

    feat: Add formatting package exports for article components and layout functions

---
+++ ai_newsletter/formatting/__init__.py
@@ -0,0 +1,15 @@
+"""Formatting package exports."""
+from ai_newsletter.formatting.components import format_summary_block, get_tags_html
+from ai_newsletter.formatting.layout import wrap_with_css, build_header, build_footer, build_empty_newsletter
+from ai_newsletter.formatting.render import format_article, build_newsletter
+
+__all__ = [
+    'format_summary_block',
+    'get_tags_html',
+    'wrap_with_css',
+    'build_header',
+    'build_footer',
+    'build_empty_newsletter',
+    'format_article',
+    'build_newsletter'
+]
